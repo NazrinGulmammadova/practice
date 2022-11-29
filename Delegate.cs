490465c64207f60b5fb48a0c7f19da8bed36124b
NazrinGulmammadova <116623519+NazrinGulmammadova@users.noreply.github.com>
2022-11-29 12:47:41 +0400

    Add files via upload

---
+++ Delegate.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine(CheckIndex(new int[] {-1,3,-2,4}, Positive));
+
+//int CheckIndex<int>(int[] arr, Check<int> check)
+int CheckIndex<T>(T[] arr, Check<T> check)
+{
+	for (int i = 0; i < arr.Length; i++)
+    {
+	  if (check(arr[i]))
+	  {
+			return i;
+	  }
+    }
+	return -1;
+}
+
+bool Positive(int element)
+{
+	if(element> 0)
+		return true;
+
+	return false;
+}
+
+
+//delegate bool Check<int>(int element);
+delegate bool Check<K>(K element);
